cd19e04d1ffb2bcb120e33116e3de7037a19f18e
Alexander Sommen <axio@axio.name>
2019-08-02 12:51:42 +0100

    Initial release

---
+++ webpasskeeper.js
@@ -0,0 +1,295 @@
+var login;
+var password;
+var timeShutdown;
+var db;
+
+(function($) {
+	$.QueryString = (function(paramsArray) {
+		let params = {};
+
+		for (let i = 0; i < paramsArray.length; ++i)
+		{
+			let param = paramsArray[i].split('=', 2);
+
+			if (param.length !== 2)
+				continue;
+
+			params[param[0]] = decodeURIComponent(param[1].replace(/\+/g, " "));
+		}
+
+		return params;
+	})(window.location.search.substr(1).split('&'))
+})(jQuery);
+
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+	console.error('Async clipboard is not available');
+	return;
+  }
+  navigator.clipboard.writeText(text);
+}
+
+function download(filename, text) {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
+}
+
+function encryptData(dataAsString, password) {
+	var encrypted = CryptoJS.AES.encrypt(dataAsString, password);
+	var obj = {
+		ciphertext: encrypted.ciphertext.toString(CryptoJS.enc.Base64),
+		iv: encrypted.iv.toString(),
+		salt: encrypted.salt.toString()
+	};
+	return JSON.stringify(obj);
+}
+
+function decryptData(data, password) {
+	var obj = JSON.parse(data);
+	var cipherParams = CryptoJS.lib.CipherParams.create({
+      ciphertext: CryptoJS.enc.Base64.parse(obj.ciphertext),
+	  iv: CryptoJS.enc.Hex.parse(obj.iv),
+	  salt: CryptoJS.enc.Hex.parse(obj.salt)
+    });
+	var decrypted = CryptoJS.AES.decrypt(cipherParams, password);
+	return decrypted.toString(CryptoJS.enc.Utf8);
+}
+
+function getEntriesFromServer(func, errFunc) {
+	$.ajax({ 
+		url: "webpasskeeper.php",
+		async: false,
+		method: "POST",
+		data: { method: "get", login: login, hashed_password: CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex) },
+		dataType: "text",
+		statusCode: {
+		    403: function() {
+		    	alert("Incorrect credentials");
+		    	errFunc();
+		    },
+		    404: function() {
+		    	alert("User is not found or credentials is not valid");
+		    	errFunc();
+		    },
+		},
+		success: function(data) {
+			var decryptedData = decryptData(data, password);
+			if (decryptedData != "") {
+				var obj = JSON.parse(decryptedData);
+				db = obj;
+				updateView();
+				func();
+			}
+			else {
+				alert("Decryption failed!");
+				errFunc();
+			}
+		},
+	});
+}
+
+function updateView() {
+	var result = "<table border=\"1\"><tr><th>Entry</th><th>Login</th><th>Password</th></tr>";
+	var searchtext = $("#txtbox_search").val();
+	for (var i in db.data) {
+		if (searchtext != "") {
+			if (i.includes(searchtext)) {
+				result += `<tr><th><a href='#' onclick=\"copyTextToClipboard('${i}');return false;\">${i}</a></th><th><a href='#' onclick=\"copyTextToClipboard('${db.data[i].login}');return false;\">${db.data[i].login}</a></th><th><a href='#' onclick=\"copyTextToClipboard('${db.data[i].password}');return false;\">${db.data[i].password}</a></th></tr>`;
+			}
+		}//<a href='#' onclick=\"copyTextToClipboard('${db.data[i].password}');return false;\">${db.data[i].password}</a>
+		else {
+			result += `<tr><th><a href='#' onclick=\"copyTextToClipboard('${i}');return false;\">${i}</a></th><th><a href='#' onclick=\"copyTextToClipboard('${db.data[i].login}');return false;\">${db.data[i].login}</a></th><th><a href='#' onclick=\"copyTextToClipboard('${db.data[i].password}');return false;\">${db.data[i].password}</a></th></tr>`;
+		}
+	}
+	result += "</table>";
+	$("#div_login").html(result);
+}
+
+function sendDataToServer() {
+	if (db != null) {
+		var dbJson = JSON.stringify(db);
+		var dbJsonEncrypted = encryptData(dbJson, password);
+		$.post("webpasskeeper.php", { 
+			method: "store", 
+			login: login, 
+			hashed_password: CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex), 
+			data: dbJsonEncrypted 
+		});
+	}
+}
+
+function registerNewUser(login, password, func) {
+	$.ajax({ 
+		url: "webpasskeeper.php",
+		async: false,
+		method: "POST",
+		data: { method: "register", login: login, hashed_password: CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex) },
+		dataType: "text",
+		statusCode: {
+		    403: function() {
+		    	alert("Please enter credentials");
+		    },
+		    402: function() {
+		    	alert("You cannot register with these credentials");
+		    },
+		},
+		success: func,
+	});
+}
+
+function initDB() {
+	return {
+		"data-modified": Math.floor(Date.now() / 1000),
+		"login": login,
+		"data": { },
+	};
+}
+
+function enableControls() {
+	$("#div_main").css("display", "inline");
+	$('#txtbox_search').focus();
+	$('#txtbox_search').on('input', updateView);
+	StartAutoLogoutTimer();
+}
+
+function OnLoginOrRegisterClick(e) {
+	e.preventDefault();
+	DisableEnterKey();
+	login = $("#login").val();
+	password = $("#password").val();
+}
+
+// set register button
+$(document).ready(function() {
+	$("#register_new_user").click(function(e) {
+		OnLoginOrRegisterClick(e);
+		registerNewUser(login, password, function(data) {
+			alert("User successfully registered");
+			db = initDB();
+			sendDataToServer();
+			updateView();
+			enableControls();
+		});
+	})
+});
+
+//set login button
+$(document).ready(function() {
+	$("#submit_login_data").click(function(e) {
+		OnLoginOrRegisterClick(e);
+		getEntriesFromServer(enableControls,
+		function() {
+//			$("#div_main").html("");
+		});
+	})
+});
+
+//set import btn
+$(document).ready(function() {
+	$("#btn_import").click(function(e) {
+		e.preventDefault();
+		$("#importExportDiv").css("display", "inline");
+	});
+	document.getElementById("importExportDiv").addEventListener("paste", handlePaste);
+});
+function handlePaste(e) {
+    var clipboardData, pastedData;
+    // Stop data actually being pasted into div
+    e.stopPropagation();
+    e.preventDefault();
+    // Get pasted data via clipboard API
+    clipboardData = e.clipboardData || window.clipboardData;
+    pastedData = clipboardData.getData('Text');
+    // Do whatever with pasteddata
+    var obj = JSON.parse(pastedData);
+    for (var i in obj) {
+    	db.data[i] = {
+    			"login": obj[i].login,
+				"password": obj[i].password,
+				"date_updated": Math.floor(Date.now() / 1000),
+    	};
+	}
+    db["data-modified"] = Math.floor(Date.now() / 1000);
+    sendDataToServer();
+    alert("Import is completed successfully");
+    $("#importExportDiv").css("display", "none");
+    updateView();
+}
+
+//set export btn
+$(document).ready(function() {
+	$("#btn_export").click(function(e) {
+		e.preventDefault();
+		download("credentials.json", JSON.stringify(db.data));
+	});
+});
+
+//add/modify button logic
+$(document).ready(function() {
+	$("#btn_modify_entry").click(function(e) {
+		e.preventDefault();
+		var entryName = $("#txtbox_modify_entry_name").val();
+		var entryLogin = $("#txtbox_modify_entry_login").val();
+		var entryPassword = $("#txtbox_modify_entry_password").val();
+		if (entryLogin == "" && entryPassword == "") {
+			delete db.data[entryName];
+		}
+		else {
+			db.data[entryName] = {
+					"login": entryLogin,
+					"password": entryPassword,
+					"date_updated": Math.floor(Date.now() / 1000),
+			};
+		}
+		db["data-modified"] = Math.floor(Date.now() / 1000);
+		sendDataToServer();
+		alert("Entry with name '" + $("#txtbox_modify_entry_name").val() + "' is added/modified");
+		$('#txtbox_search').trigger('input');
+	});
+});
+
+function DisableEnterKey() {
+	$(document).keypress(function(event) {
+		if (event.which == '13') {
+			event.preventDefault();
+		}
+	});
+}
+			
+function StartAutoLogoutTimer() {
+	timeShutdown = new Date().getTime() + 10 * 60 * 1000; // 10 min
+	setTimeout(function() {
+		$("#div_main").html("<strong>Your session has been automatically ended</strong>");
+		$("#div_login").html("");
+		login = null;
+		password = null;
+		db = null;
+	}, 10 * 60 * 1000); // 10 min
+	setInterval(function() {
+		var distance = timeShutdown - new Date().getTime();
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		$("#search_form_legend").html("Management | Time to session end: " + minutes + "m " + seconds + "s");
+	}, 1000);
+}
+
+// setup login if it is passed in query string
+$(document).ready(function() {
+	if ($.QueryString["file"] != null && $.QueryString["file"] != "") {
+		$("#login").val($.QueryString["file"]);
+		$("#password").focus();
+	}
+	else {
+		$("#login").focus();
+	}
+});
---
+++ webpasskeeper.php
@@ -0,0 +1,91 @@
+<?php
+
+$userfilesPrefix = "webpasskeeper/";
+if (!file_exists($userfilesPrefix)) {
+    mkdir($userfilesPrefix, 0700, true);
+}
+
+function GetPostField($field) {
+	$v = htmlspecialchars($_POST[$field]);
+	if ($v === "") {
+		return null;
+	}
+	else {
+		return $v;
+	}
+}
+
+/**
+ * Generate a random string, using a cryptographically secure 
+ * pseudorandom number generator (random_int)
+ * 
+ * For PHP 7, random_int is a PHP core function
+ * For PHP 5.x, depends on https://github.com/paragonie/random_compat
+ * 
+ * @param int $length      How many characters do we want?
+ * @param string $keyspace A string of all possible characters
+ *                         to select from
+ * @return string
+ */
+function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+    $pieces = [];
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    for ($i = 0; $i < $length; ++$i) {
+        $pieces []= $keyspace[random_int(0, $max)];
+    }
+    return implode('', $pieces);
+}
+
+$postMethod = GetPostField("method");
+if ($postMethod === "register") {
+	$login = GetPostField("login");
+	$hashed_password = GetPostField("hashed_password");
+	if ($login === null or $hashed_password === null) {
+		http_response_code(403); // Incorrect credentials
+		die();
+	}
+	$newFileName = hash("sha256", $login . $hashed_password);
+	if (file_exists($userfilesPrefix . $newFileName)) {
+		http_response_code(402); // File already exist
+		die();
+	}
+	file_put_contents($userfilesPrefix . $newFileName, "");
+	chmod($userfilesPrefix . $newFileName, 0600);
+	echo "OK";
+	die();
+}
+elseif (GetPostField("method") === "get") {
+	$login = GetPostField("login");
+	$hashed_password = GetPostField("hashed_password");
+	if ($login === null or $hashed_password === null) {
+		http_response_code(403); // Incorrect credentials
+		die();
+	}
+	$file_name = hash("sha256", $login . $hashed_password);
+	$file_contents = file_get_contents($userfilesPrefix . $file_name);
+	if ($file_contents === false) {
+		http_response_code(404); // Secure file not found
+		die();
+	}
+	echo $file_contents;
+}
+elseif (GetPostField("method") === "store") {
+	$login = GetPostField("login");
+	$hashed_password = GetPostField("hashed_password");
+	if ($login === null or $hashed_password === null) {
+		http_response_code(403); // Incorrect credentials
+		die();
+	}
+	$file_name = hash("sha256", $login . $hashed_password);
+	if (file_exists($userfilesPrefix . $file_name) === false) {
+		http_response_code(404); // Secure file not found
+		die();
+	}
+	file_put_contents($userfilesPrefix . $file_name, $_POST["data"]);
+	chmod($userfilesPrefix . $file_name, 0600);
+	echo "OK";
+	die();
+}
+
+
+?>
